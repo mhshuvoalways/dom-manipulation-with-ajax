32d10a8af438aee145e67cee14be79e82b61a1cb
mhshuvoit <mhshuvoit@gmail.com>
2020-10-08 21:31:44 +0600

    first commit

---
+++ client/webpack.config.js
@@ -0,0 +1,46 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'js/index.js',
+    // publicPath: '/assets/',
+  },
+  mode: 'development',
+  devServer: {
+    port: 2000,
+    open: true,
+    compress: true,
+    contentBase: path.join(__dirname, 'public')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    }),
+    new LodashModuleReplacementPlugin,
+    new MiniCssExtractPlugin()
+  ]
+};
+
+module.exports = config;
